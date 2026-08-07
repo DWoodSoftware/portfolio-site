e7f7029dadcef2cde4fb3137591079c08ed45188
DavidWood2001 <davidwood1301@gmail.com>
2026-08-07 19:07:02 +0100

    chore: initialise Vite portfolio structure

---
+++ src/main.tsx
@@ -0,0 +1,16 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './styles/global.css'
+import { App } from './app/App.tsx'
+
+const rootElement = document.getElementById("root");
+
+if (!rootElement) {
+  throw new Error("Unable to initialize application: #root element not found.")
+}
+
+createRoot(rootElement).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
